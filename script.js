442dd7050f35ef303c677959a12f231878274509
Clarawita <clarawita28@gmail.com>
2024-07-10 18:09:10 +0700

    update the latest code

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-// git config --global user.email "clarawita28@gmail.com"
-// git config --global user.name "Clarawita"
+function changeBg(){
+  var navbar = document.querySelector('.navbar');
+  var scrollValue = window.scrollY;
+  // console.log(scrollValue);
+  if (scrollValue < 100){
+    navbar.classList.remove('bgColor');
+  }else {
+    navbar.classList.add('bgColor');
+  }
+}
+
+window.addEventListener('scroll', changeBg);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
